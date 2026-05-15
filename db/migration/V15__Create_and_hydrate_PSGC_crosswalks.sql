aaba72ad566ac7829e425721c7d41506965d8665
Gabriel Virrey <85rkocqp3@mozmail.com>
2026-05-15 18:16:34 +0800

    Only make correspondence table for PSGC municipalities

--- db/migration/V15__Create_and_hydrate_PSGC_crosswalks.sql
+++ db/migration/V15__Create_and_hydrate_PSGC_crosswalks.sql
@@ -1,27 +1,18 @@
-create table psgc_region_correspondence (
-    correspondence_code char(9) primary key,
-    region_code int not null,
+create table psgc_municipality_correspondence
+(
+    correspondence_code char(6) primary key,
+    municipality_id     int not null,
 
-    constraint fk_psgc_region_correspondence_region_id foreign key (region_code) references psgc_regions(code)
+    constraint fk_psgc_municipality_correspondence_mun_id foreign key (municipality_id) references psgc_municipalities (id)
 );
 
-create table psgc_province_correspondence (
-    correspondence_code char(9) primary key,
-    province_id int not null,
-
-    constraint fk_psgc_province_correspondence_province_id foreign key (province_id) references psgc_provinces(id)
-);
-
-create table psgc_municipality_correspondence (
-    correspondence_code char(9) primary key,
-    municipality_id int not null,
-
-    constraint fk_psgc_municipality_correspondence_mun_id foreign key (municipality_id) references psgc_municipalities(id)
-);
-
-create table psgc_barangay_correspondence (
-    correspondence_code char(9) primary key,
-    barangay_id int not null,
-
-    constraint fk_psgc_barangay_correspondence_barangay_id foreign key (barangay_id) references psgc_barangays(id)
-);
+insert into psgc_municipality_correspondence (correspondence_code, municipality_id)
+select left(correspondence_code, 6), pm.id
+from psgc_raw_data as prd
+         join psgc_provinces pp
+              on left(prd.psgc_rev_1, 2)::int = pp.region_code and substring(prd.psgc_rev_1 from 3 for 3)::int = pp.code
+         join psgc_municipalities as pm
+              on pp.id = pm.province_id and substring(prd.psgc_rev_1 from 6 for 2)::int = pm.code
+where prd.correspondence_code is not null
+  and (prd.geographic_level = 'City' or prd.geographic_level = 'Mun' or
+       prd.geographic_level = 'SubMun');
